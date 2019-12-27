e24e235dccd38f8cc91aecd9a0fee8a2f80151ab
Ryota Ikezawa <pavegy@gmail.com>
2019-12-27 18:24:04 +0900

    :tada: Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+const Page = () => (
+  <p>This is the about page</p>
+);
+
+export default Layout(Page);
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+const Page = () => (
+  <p>Hello Next.js</p>
+);
+
+export default Layout(Page);
